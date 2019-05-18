6ed798cb046f64631b6480e44a8614d24eb82cba
Phumphathai <ouoam2555@gmail.com>
2019-05-18 17:59:05 +0700

    Can send road data and receive siren data

--- device/src/main.cpp
+++ device/src/main.cpp
@@ -6,14 +6,70 @@
 // Set these to run example.
 #define FIREBASE_HOST "smart-traffic-light-ec485.firebaseio.com"
 #define FIREBASE_AUTH "S6StyQdeUErhCDxL4ua29jG253DuxEJeSQCWImap"
-#define WIFI_SSID "TP-LINK_B3D1F6"
-#define WIFI_PASSWORD "20818043"
+#define WIFI_SSID "CE-ESL"
+#define WIFI_PASSWORD "ceeslonly"
 
-unsigned long long last = 0;
+#define NUMBER_OF_SHIFT_CHIPS 4
+#define DATA_WIDTH NUMBER_OF_SHIFT_CHIPS * 8
+#define BYTES_VAL_T unsigned int
+
+int ploadPin = D3; // Connects to Parallel load pin the 165
+int dataPin = D2;  // Connects to the Q7 pin the 165
+int clockPin = D0; // Connects to the Clock pin the 165
+
+BYTES_VAL_T pinValues;
+BYTES_VAL_T oldPinValues;
+
+BYTES_VAL_T read_shift_regs()
+{
+  bool bitVal;
+  BYTES_VAL_T bytesVal = 0;
+
+  digitalWrite(ploadPin, LOW);
+  digitalWrite(ploadPin, HIGH);
+
+  for (int i = 0; i < DATA_WIDTH; i++)
+  {
+    bitVal = digitalRead(dataPin);
+
+    bytesVal |= (bitVal << ((DATA_WIDTH - 1) - i));
+
+    digitalWrite(clockPin, HIGH);
+    digitalWrite(clockPin, LOW);
+  }
+
+  return (bytesVal);
+}
+
+void display_pin_values()
+{
+  char a[40];
+  for (int i = 0; i < DATA_WIDTH; i++)
+  {
+    a[i] = ((pinValues >> i) & 1) + '0';
+    Serial.print((pinValues >> i) & 1);
+  }
+  Serial.print("\r\n");
+
+  a[32] = 0;
+  Firebase.setString("DEBUG/road", a);
+  Firebase.setInt("data/road", pinValues);
+}
 
 void setup()
 {
   Serial.begin(9600);
+
+  /* Initialize our digital pins...
+    */
+  pinMode(ploadPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(dataPin, INPUT);
+
+  digitalWrite(clockPin, LOW);
+  digitalWrite(ploadPin, HIGH);
+
+  pinMode(D1, OUTPUT);
 
   // connect to wifi.
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
@@ -28,22 +84,47 @@ void setup()
   Serial.println(WiFi.localIP());
 
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.stream("control");
 
-  last = millis();
+  pinValues = read_shift_regs();
+  display_pin_values();
+  oldPinValues = pinValues;
 }
 
 void loop()
 {
-  if (millis() >= last)
+  if (Firebase.failed())
   {
-    Firebase.setInt("road/0", millis());
-    if (Firebase.failed())
+    Serial.println("Firebase error");
+    Serial.println(Firebase.error());
+  }
+
+  pinValues = read_shift_regs();
+  if (pinValues != oldPinValues)
+  {
+    display_pin_values();
+    oldPinValues = pinValues;
+  }
+
+  if (Firebase.available())
+  {
+    FirebaseObject event = Firebase.readEvent();
+    String eventType = event.getString("type");
+    eventType.toLowerCase();
+
+    if (eventType == "put")
     {
-      Serial.print("setting1 /number failed:");
-      Serial.println(Firebase.error());
-      return;
+      String path = event.getString("path");
+
+      Serial.print("Stream Event : ");
+      Serial.print(path);
+      Serial.print(" ");
+      Serial.println(event.getJsonVariant("data").as<char *>());
+
+      if (path == "/siren")
+      {
+        digitalWrite(D1, event.getBool("data"));
+      }
     }
-    Serial.println("SAVE");
-    last += 1000;
   }
 }
